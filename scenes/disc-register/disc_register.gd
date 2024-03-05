659cde8b475327292ad326ad70000f3eaa21b803
Alex Hall <analytics@alex-hall.co.uk>
2024-03-05 23:20:02 +0000

    added rapid access registers and main memory

--- scenes/disc-register/disc_register.gd
+++ scenes/disc-register/disc_register.gd
@@ -3,12 +3,15 @@ extends Node2D
 @export var register_label: String
 @export var register_value: int
 @export var num_registers: int
+@export var num_sectors: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	var label = get_node("Label")
 	label.text = register_label
+
+	
 
 
 
@@ -17,6 +20,8 @@ func _process(delta):
 	var register_binary = value_to_binary(register_value)
 	var register_binary_label = get_node("value_binary")
 	register_binary_label.text = register_binary
+	
+	
 	
 
 func value_to_binary(value):
@@ -31,3 +36,9 @@ func value_to_binary(value):
 			ret_str = str(value&1) + ret_str
 			value = (value>>1)
 		return ret_str
+		
+		
+
+		
+
+	
